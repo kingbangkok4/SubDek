9812aea11639943834df12116261ba614bcbc9ff
kingbangkok4 <kingbangkok4@gmail.com>
2016-05-26 10:12:58 +0700

    Add picture

--- app/src/main/java/com/app/subdek/AbountActivity.java
+++ app/src/main/java/com/app/subdek/AbountActivity.java
@@ -3,16 +3,27 @@ package com.app.subdek;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
-public class AbountActivity extends Activity{
+public class AbountActivity extends Activity {
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abount);
 
-        final Handler handler = new Handler();
+        btnBack = (Button)findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), MainMenuActivity.class);
+                startActivity(i);
+            }
+        });
+
+      /*  final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,6 +31,7 @@ public class AbountActivity extends Activity{
                 startActivity(i);
                 finish();
             }
-        }, 5000);
+        }, 5000);*/
+
     }
 }
